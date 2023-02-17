bb8dff5aeb16b2457f11c60c2985e6b20e4ed897
Brandon Watson <bwatson@kompartners.com>
2023-02-17 09:25:11 -0700

    multistep commit to deal with upper lower case issue on github

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,27 +1,4 @@
 export type AmplifyDependentResourcesAttributes = {
-  "api": {
-    "cmabuilderCMARest": {
-      "ApiId": "string",
-      "ApiName": "string",
-      "RootUrl": "string"
-    },
-    "cmabuilderPropertyRest": {
-      "ApiId": "string",
-      "ApiName": "string",
-      "RootUrl": "string"
-    }
-  },
-  "auth": {
-    "cmabuilderda9f7bb4": {
-      "AppClientID": "string",
-      "AppClientIDWeb": "string",
-      "IdentityPoolId": "string",
-      "IdentityPoolName": "string",
-      "UserPoolArn": "string",
-      "UserPoolId": "string",
-      "UserPoolName": "string"
-    }
-  },
   "function": {
     "cmabuilderCMARest": {
       "Arn": "string",
