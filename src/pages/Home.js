0d411e948e32ef5c9ab5f59135c60f3b98a5b30e
Brandon Watson <bwatson@kompartners.com>
2023-02-20 19:01:05 -0700

    Data init mostly works - stuck on the hasMany operator for the model and how to insert relational data for the CMA listing and comps

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,6 +13,9 @@ import { Button } from '@mui/material'
 import { DataStore } from 'aws-amplify'
 import { CMA, Property } from '../models/index'
 
+//TEST DATA INIT
+import INIT_DATA from '../dynamo/amplify_init_data'
+
 async function CreateNewCma(cma)
 {
     await DataStore.save(
@@ -23,20 +26,86 @@ async function CreateNewCma(cma)
         )
 }
 
+async function InitData(allProperties, user)
+{
+    //This is sample data what will be removed
+    const listingProperty = allProperties.slice(0,1)
+    const compProperties = allProperties.slice(1)
+
+    const targets = listingProperty.map(property => [property.pk, property.sk])
+    const comparables = compProperties.map(property => [property.pk, property.sk])
+    // console.log(targets[0])
+    // console.log(comparables)
+
+    const cma = {
+        pk: 'CMA',
+        sk: user.attributes.email + '#' + new Date(),
+        cma_label: allProperties[0]["sk"],
+        client_name: "Jenkins",
+        properties: targets[0],
+        comparables: comparables
+    }
+
+    // console.log(cma)
+    //create the objects here and then write to datastore
+    // for (let i=0; i < allProperties.length; i++)
+    // {
+    //     //console.log("inserting", allProperties[i].sk)
+    //     try {
+    //         await DataStore.save(
+    //             new Property ({
+    //                 "pk": allProperties[i].pk,
+    //                 "sk": allProperties[i].sk,
+    //                 "above_grade": allProperties[i].above_grade,
+    //                 "finished_basement_sqft": allProperties[i].finished_basement_sqft,
+    //                 "list_price": allProperties[i].list_price,
+    //                 "main_sqft": allProperties[i].main_sqft,
+    //                 "num_baths": allProperties[i].num_baths,
+    //                 "num_beds": allProperties[i].num_beds,
+    //                 "sale_price": allProperties[i].sale_price,
+    //                 "total_sqft": allProperties[i].total_sqft,
+    //                 "unfinished_basement_sqft": allProperties[i].unfinised_basement_sqft,
+    //                 "upper_sqft": allProperties[i].upper_sqft,
+    //                 "UUID": allProperties[i].uuid}
+    //             )
+    //         )
+    //         console.log("Success inserting propert");
+    //     } catch (error) {
+    //         console.log("Error saving property", error);
+    //     }
+    // }
+    // }
+
+    try {
+        await DataStore.save(
+            new CMA ({
+                "pk": cma.pk,
+                "sk": cma.sk,
+                "cma_label": cma.cma_label,
+                "client_name": cma.client_name,
+                "properties": cma.properties,
+                "comparables": cma.comparables
+            })
+        )
+        // console.log(cma.comparables)
+        console.log("success inserting CMA")
+    } catch (error) {
+        console.log("error inserting CMA", error)
+    }
+}
+
 function Home({signOut, user}) {
     //I will need to use react query but for now just use fetch against the API
     const [cmaList, setCmaList] = useState()
 
-    function newClickHandler()
+    function initDataHandler()
     {
-        //This is sample data what will be removed
-        const testCma = {
-            pk: 'CMA',
-            sk: user.attributes.email + '#' + new Date()
-        }
-        CreateNewCma(testCma)
+        InitData(INIT_DATA["properties"], user)
+    }
 
-        console.log(`new property created: {0}`, testCma)
+    function newCmaClickHandler()
+    {
+        console.log(`New CMA button clicked`)
     }
     
     return (
@@ -47,7 +116,12 @@ function Home({signOut, user}) {
         <div>
             <Button
                 variant='contained'
-                onClick = {newClickHandler}>Create New CMA</Button>
+                onClick = {initDataHandler}>Initialize
+            </Button>
+            <Button
+                variant='contained'
+                onClick = {newCmaClickHandler}>Create New CMA
+            </Button>
         </div>
         <div>
             <Typography variant='h4'>Created CMAs</Typography>
